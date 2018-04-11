363886ec9755cadbbe1d6e854828fa317881c482
doommchips <bradley.walker@booking.com>
2018-04-11 18:10:07 +0200

    optimise drawTrack function

--- 0-js/track.js
+++ 0-js/track.js
@@ -58,16 +58,19 @@ function rowColToArrayIndex(col, row) {
 }
 
 function drawTracks() {
+    var arrayIndex = 0;
+    var drawTileX  = 0;
+    var drawTileY  = 0;
     for (var eachRow=0; eachRow<TRACK_ROWS; eachRow++) {
         for (var eachCol=0; eachCol<TRACK_COLS; eachCol++) {
-
-            var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
             var tileKindHere = trackGrid[arrayIndex];
-
             var useImg = trackPics[tileKindHere];
+            canvasContext.drawImage(useImg, drawTileX, drawTileY);
 
-            canvasContext.drawImage(useImg, TRACK_W*eachCol, TRACK_H*eachRow);
-
+            drawTileX += TRACK_W;
+            arrayIndex++;
         }       // end of for each track col
+        drawTileX = 0; // reset row
+        drawTileY += TRACK_H;
     }           // end of for each track row
 }               // end of draw tracks func
